260a80ec03a2febcc37e2ed5b43deaf52d0b7f99
Daniele Deltodesco <deaddread666@gmail.com>
2019-11-06 13:30:44 +0530

    created routes

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express'),
-    app = express();
-    port = process.env.PORT || 3000;
+    app = express(),
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
+
+var todoRoutes = require("./routes/todos");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
-  res.send("hi there from express!")
+  res.send("Hellow from the root route")
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
   console.log('App is running on port: ' + port);
-})
+});
